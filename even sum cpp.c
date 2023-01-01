50389bf00dc3a2475241fb2cf5c9409e06a35820
Istiak Ahmed Khan <istiakahmedjoy99@gmail.com>
2023-01-02 00:19:46 +0600

    codeforces from march 2022

---
+++ even sum cpp.c
@@ -0,0 +1,18 @@
+#include<bits\stdc++.h>
+using namespace std;
+int main()
+{
+    int a,i,sum=0,n;
+    cin>>n;
+    a=n*2;
+    for(i=2;i<=a;i+=2)
+    {
+
+        sum+=i;
+    }
+cout<<sum<<
+
+
+    return 0;
+}
+
---
+++ file.cpp
@@ -0,0 +1,45 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef long double ld;
+const ld PI = 2*acosl(0.0);
+const int inf=2e5+7;
+const int mxN=3000000;
+const int mod=998244353;
+#define REP(i,a,b) for (int i = a; i <b; i++)
+#define SQ(a) a*a
+#define speed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define reset(a) memset(a,0,sizeof a)
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define abs(a) (a<0?-(a):a)
+#define debug1(x)  cout << #x << "=" << x << endl
+#define debug2(x, y)  cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define digit2(x) floor((log2(x)))
+#define digit2(x) floor((log2(x)))
+#define sc(a) scanf("%d",&a)
+#define pf(a) printf("%d\n",a)
+#define DEBUG 0
+int main ()
+{
+	int i,j,k=0,n;
+	cin>>n;
+	for(i=1;i<=n;i++){
+		if (i%2!=0)
+		{
+			k=k+i*-1;
+		}
+		else{
+			k=k+i;
+		}
+	}
+	cout<<k<<endl;
+
+    return 0;
+}
