fdc067b6bc3e300fb4cd4c544fe824af233fe36d
Istiak Ahmed Khan <istiakahmedjoy99@gmail.com>
2023-01-18 05:40:57 +0600

    codeforces
    Brute force, greedy, dp

---
+++ cf 996A.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef long double ld;
+const ld PI = 2*acosl(0.0);
+const int inf=2e5+7;
+const int mxN=3000000;
+const int mod=998244353;
+#define REP(i,a,b) for (int i = a; i <b; i++)
+#define SQ(a) a*a
+#define speed ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
+#define pb push_back
+#define reset(a) memset(a,0,sizeof a)
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) (a/gcd(a,b)*b)
+#define abs(a) (a<0?-(a):a)
+#define debug1(x)  cout << #x << "=" << x << endl
+#define debug2(x, y)  cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define digit2(x) floor((log2(x)))
+#define digit2(x) floor((log2(x)))
+#define sc(a) scanf("%d",&a)
+#define pf(a) printf("%d\n",a)
+#define DEBUG 0
+int main ()
+{	
+	int t,c,count=0;
+	cin>>t;
+	int a[]={100,20,10,5,1};
+	for(int i=0;i<5;i++){
+		count+=t/a[i];
+		t=t%a[i];
+	}
+	cout<<count<<endl;
+
+
+
+
+
+
+	
+
+
+    return 0;
+}
+
